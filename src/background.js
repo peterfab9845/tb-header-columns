baff3413b8c52cb2add44d290f0f1b40d573be50
Peter Fabinski <peter@peterfab.com>
2022-02-01 05:57:41 -0500

    Add the pref whenever background.js is loaded

--- src/background.js
+++ src/background.js
@@ -6,7 +6,7 @@ async function init() {
 init();
 
 async function addCustomDBHeader() {
-  // Add the X-Original-To header to customDBHeaders
+  // Add the X-Original-To header to customDBHeaders if it's missing
   let customDBHeaders = await messenger.LegacyPrefs.getPref("mailnews.customDBHeaders", "");
   if (!customDBHeaders.toLowerCase().includes("x-original-to")) {
     // the DB entry is case-insensitive, all are used in lowercase
@@ -14,24 +14,5 @@ async function addCustomDBHeader() {
     await messenger.LegacyPrefs.setPref("mailnews.customDBHeaders", customDBHeaders);
   }
 }
-messenger.runtime.onInstalled.addListener(async (details) => {
-  addCustomDBHeader();
-});
-
-/* Did not work right - "Error: Can't find profile directory." Not triggered
-   soon enough? I removed the management permission since I'm not using it
-   anywhere else.
-   This functionality is currently in onShutdown() in the CustomColumns API
-async function removeCustomDBHeader() {
-  // Remove the X-Original-To header from customDBHeaders
-  let customDBHeaders = await messenger.LegacyPrefs.getPref("mailnews.customDBHeaders", "");
-  customDBHeaders = customDBHeaders.replace(/ *x-original-to/i, "");
-  await messenger.LegacyPrefs.setPref("mailnews.customDBHeaders", customDBHeaders);
-}
-messenger.management.onUninstalled.addListener(async (info) => {
-  if (info.id == messenger.runtime.id) {
-    removeCustomDBHeader();
-  }
-});
-*/
+addCustomDBHeader();
 
