ce6ee554395d2f05f7ebf0df32e90b04b1add252
Peter Fabinski <git@peterfab.com>
2022-07-02 20:06:55 -0400

    Use new API, add prototype options

--- src/background.js
+++ src/background.js
@@ -1,58 +1,29 @@
 "use strict";
 
 async function init() {
-  // set up customDBHeaders
-  // TODO
-  //await addCustomDBHeader("X-Original-To");
-  //messenger.ex_runtime.onDisable.addListener(removeCustomDBHeader.bind(null, "X-Original-To"));
-
-  // add the column
-  let testTree = {
-    "nodeType": "first",
-    "children": [
-      {
-        "nodeType": "regex",
-        "pattern": "@.*",
-        "flags": "",
-        "replacement": "",
-        "child": {
-          "nodeType": "first",
-          "children": [
-            {
-              "nodeType": "header",
-              "headerName": "X-Delivered-to"
-            },
-            {
-              "nodeType": "header",
-              "headerName": "X-Original-To"
-            }
-          ]
-        }
-      },
-      {
-        "nodeType": "literal",
-        "literalString": "Unknown"
-      }
-    ]
-  };
-  messenger.HeaderColumns.registerColumn("headerColumn-test", "test", "Sort by test", testTree, false);
-}
-init();
-
-// Add a header to customDBHeaders if it's missing
-async function addCustomDBHeader(headerName) {
-  let customDBHeaders = await messenger.LegacyPrefs.getPref("mailnews.customDBHeaders", "");
-  if (!customDBHeaders.toLowerCase().includes(headerName.toLowerCase())) {
-    customDBHeaders += " " + headerName;
-    await messenger.LegacyPrefs.setPref("mailnews.customDBHeaders", customDBHeaders);
+  let tree = (await browser.storage.sync.get("tree")).tree;
+  if (tree) {
+    registerTestTree(tree);
   }
 }
 
-// Remove a header from customDBHeaders if it's present
-async function removeCustomDBHeader(headerName) {
-  let customDBHeaders = await messenger.LegacyPrefs.getPref("mailnews.customDBHeaders", "");
-  let regex = new RegExp(` *${headerName}`, "i");
-  customDBHeaders = customDBHeaders.replace(regex, "");
-  await messenger.LegacyPrefs.setPref("mailnews.customDBHeaders", customDBHeaders);
+function onStorageChanged(changes, areaName) {
+  if (areaName == "sync" && "tree" in changes) {
+    let tree = changes.tree.newValue;
+    if (tree) {
+      registerTestTree(tree);
+    }
+  }
 }
+
+function registerTestTree(tree) {
+  let testOptions = {
+    "sortNumeric": false,
+    "useDBHeaders": false
+  };
+  messenger.HeaderColumns.registerColumn("headerColumn-test", "test", "Sort by test", tree, testOptions);
+}
+
+browser.storage.onChanged.addListener(onStorageChanged);
+init();
 
---
+++ src/options.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const defaultTree = {
+  "nodeType": "first",
+  "children": [
+    {
+      "nodeType": "regex",
+      "pattern": "@.*",
+      "flags": "",
+      "replacement": "",
+      "child": {
+        "nodeType": "first",
+        "children": [
+          {
+            "nodeType": "header",
+            "headerName": "X-Delivered-to"
+          },
+          {
+            "nodeType": "header",
+            "headerName": "X-Original-To",
+            "headerIndex": 0
+          }
+        ]
+      }
+    },
+    {
+      "nodeType": "literal",
+      "literalString": "Unknown"
+    }
+  ]
+};
+
+function init() {
+  document.querySelector("#treeForm").addEventListener("submit", saveStorage);
+  document.querySelector("#resetButton").addEventListener("click", resetStorage);
+  loadStorage();
+}
+
+function saveStorage(e) {
+  e.preventDefault();
+
+  let treeText = document.querySelector("#treeInput").value;
+  if (!treeText) {
+    browser.storage.sync.remove("tree");
+  } else {
+    let tree = JSON.parse(treeText);
+    if (tree) {
+      browser.storage.sync.set({
+        "tree": tree
+      });
+    }
+  }
+}
+
+async function loadStorage() {
+  let tree = (await browser.storage.sync.get("tree")).tree;
+  showTree(tree);
+}
+
+function resetStorage() {
+  browser.storage.sync.set({
+    "tree": defaultTree
+  });
+}
+
+function onStorageChanged(changes, areaName) {
+  if (areaName == "sync" && "tree" in changes) {
+    showTree(changes.tree.newValue);
+  }
+}
+
+function showTree(tree) {
+  let treeText = tree ? JSON.stringify(tree, null, "  ") : "";
+  document.querySelector("#treeInput").value = treeText;
+}
+
+document.addEventListener("DOMContentLoaded", init);
+browser.storage.onChanged.addListener(onStorageChanged);
+
