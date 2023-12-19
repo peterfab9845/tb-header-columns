6d728f7bc017d91dba7315c84efbacc45c6bf34b
Peter Fabinski <git@peterfab.com>
2023-12-19 01:36:37 -0500

    Update for API rev 1.3

--- src/background.js
+++ src/background.js
@@ -32,11 +32,11 @@ function registerTestTree(tree) {
     addedColumn = false;
   }
 
-  browser.mailTabs.onCellEntriesShown.addListener(updateCellEntries); // async
+  browser.mailTabs.onCellEntriesShown.addListener(updateCellEntries);
 
   browser.mailTabs.addColumn("headerColumn", {
-    "sortable": true,
     "name": "Header Column",
+    "sortable": "bySortKey",
     "hidden": false,
     "resizable": true
   });
@@ -58,12 +58,14 @@ async function updateCellEntries(columnId, entries) {
 
 async function getCellValue(messageId) {
   let msg = await messenger.messages.getFull(messageId);
-  let parseResult = parse(parseTree, msg.headers);
+  let cellText = parse(parseTree, msg.headers);
+  let sortKey = floatToLongOrder(parseFloat(cellText));
   let entryData = {
     "messageId": messageId,
     "data": {
-      "text": parseResult
-      // TODO: sort
+      "text": cellText,
+      "sort": sortKey,
+      "icon": null
     }
   };
   return entryData;
@@ -101,6 +103,41 @@ function parse(node, headers) {
       return "";
   }
   return "";
+}
+
+function floatToLongOrder(val) {
+  // Map float to long, preserving order. This takes advantage of the fact
+  // that the binary value of least significant 31 bits of an IEEE-754 float
+  // has an ordered correspondence with the absolute magnitude of the number.
+
+  // Sort non-numbers before numbers
+  if (!isFinite(val)) {
+    // 0 can only result from 0xffffffff, which corresponds to one of the
+    // many possible representations of NaN. This one appears unused.
+    return 0;
+  }
+
+  // Turn the float into a uint32 to allow bitwise operations.
+  let view = new DataView(new ArrayBuffer(4));
+  view.setFloat32(0, val);
+  let bits = view.getUint32(0);
+
+  // There are two problems we need to correct:
+  // 1. Negatives sort in reverse order. (More-negative numbers have a higher
+  //      unsigned binary value than less-negative numbers.)
+  // 2. Negatives sort before positives. (Negatives start with a 1).
+  //
+  // To address #1, we can invert the non-sign bits if the number is negative.
+  // This reverses the ordering within the negative range.
+  // To address #2, we can invert the sign bit unconditionally. This swaps
+  // the negatives and positives in the unsigned integer range.
+  //
+  // Here is how these two are accomplished:
+  // First, arithmetic right-shift the value by 31 to produce 32 bits of 1s
+  // for a negative number and 32 bits of 0s for a positive number. Then, OR
+  // this with 0x80000000 to set the MSB to 1 unconditionally. Finally, XOR
+  // the resulting mask with the original value to do #1 and #2 simultaneously.
+  return bits ^ ((bits >> 31) | (1 << 31));
 }
 
 browser.storage.onChanged.addListener(onStorageChanged);
